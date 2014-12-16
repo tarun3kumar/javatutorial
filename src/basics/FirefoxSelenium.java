493f4948778da8c620cde9968648419ecfe6ab48
tarun <tarun.kumar.bhadauria@zalando.de>
2014-12-16 14:53:19 +0100

    add more examples

--- src/basics/FirefoxSelenium.java
+++ src/basics/FirefoxSelenium.java
@@ -21,6 +21,24 @@ public class FirefoxSelenium {
         // local variable
         int temp = 0;
 
+        FirefoxSelenium firefoxSelenium1 = new FirefoxSelenium();
+        FirefoxSelenium firefoxSelenium2 = new FirefoxSelenium();
+
+        firefoxSelenium1.searchResult = "for instance firefoxSelenium1";
+        firefoxSelenium2.searchResult = "for instance firefoxSelenium2";
+
+        System.out.println("######################################");
+        System.out.println("######################################");
+        System.out.println("Access instance variable");
+        System.out.println("First object state: " + firefoxSelenium1.searchResult);
+        System.out.println("Second object state: " + firefoxSelenium2.searchResult);
+
+        System.out.println("######################################");
+        System.out.println("######################################");
+        System.out.println("Access class variable");
+        System.out.println(FirefoxSelenium.browser);
+
+        // Array
         String[] searchResultLinks = new String[2];
 
         FirefoxDriver firefoxDriver = new FirefoxDriver();
@@ -28,6 +46,7 @@ public class FirefoxSelenium {
 
         firefoxDriver.get("http://www.google.com");
 
+        // methods accepting by parameter
         firefoxDriver.findElement(By.name("q")).sendKeys("selenium HQ");
         firefoxDriver.findElement(By.name("btnG")).click();
 
@@ -64,6 +83,9 @@ public class FirefoxSelenium {
                 break;
             }
         }
+
+        System.out.println("######################################");
+        System.out.println("######################################");
 
         firefoxDriver.close();
         firefoxDriver.quit();
