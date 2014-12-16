802c420182b35992dfb3d09ad03292f2ef0daa53
tarun <tarun.kumar.bhadauria@zalando.de>
2014-12-16 14:39:40 +0100

    add test classes

--- src/basics/FirefoxSelenium.java
+++ src/basics/FirefoxSelenium.java
@@ -1,5 +1,7 @@
 package basics;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -22,17 +24,49 @@ public class FirefoxSelenium {
         String[] searchResultLinks = new String[2];
 
         FirefoxDriver firefoxDriver = new FirefoxDriver();
-        firefoxDriver.get("www.google.com");
+        firefoxDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        firefoxDriver.get("http://www.google.com");
+
         firefoxDriver.findElement(By.name("q")).sendKeys("selenium HQ");
         firefoxDriver.findElement(By.name("btnG")).click();
 
         // watch first selenium training video http://www.seleniumtests.com/p/free-selenium-training.html
         // for more on element locators
-        searchResultLinks[0] = firefoxDriver.findElement(By.cssSelector(".q.qs")).getText();
-        searchResultLinks[1] = firefoxDriver.findElement(By.id("hdtb_more")).getText();
+        searchResultLinks[0] = firefoxDriver.findElement(By.linkText("Videos")).getText();
+        searchResultLinks[1] = firefoxDriver.findElement(By.linkText("News")).getText();
 
+        System.out.println("######################################");
+        System.out.println("######################################");
+        System.out.println("Access array elements");
         System.out.println("First Link: " + searchResultLinks[0]);
-        System.out.println("First Link: " + searchResultLinks[1]);
+        System.out.println("Second Link: " + searchResultLinks[1]);
+
+        System.out.println("######################################");
+        System.out.println("######################################");
+        System.out.println("Conditional Flow Statement");
+        if (firefoxDriver.findElement(By.linkText("Videos")).isDisplayed()) {
+            System.out.println("Web Link is present");
+        }
+
+        if (firefoxDriver.findElement(By.id("resultStats")).getText().equals("microsoft")) {
+            System.out.println("microsoft is present");
+        } else {
+            System.out.println("microsoft is not present");
+        }
+
+        System.out.println("######################################");
+        System.out.println("######################################");
+        System.out.println("For loop and break statement");
+        for (String str : searchResultLinks) {
+            System.out.println(str);
+            if (str.equals("Videos")) {
+                break;
+            }
+        }
+
+        firefoxDriver.close();
+        firefoxDriver.quit();
 
     }
 
